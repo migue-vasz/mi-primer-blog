1c070dbae050c8c3eaaefd1037c7cd934e511bb9
miguel vasquez <miguevasra@gmail.com>
2019-09-02 13:20:59 -0600

    archivo modificado

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from .models import Publicacion
 # Create your views here.
 def listar_pub(request):
     pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
-    return render(request, 'blog/templates/blog/listar_pub.html', {'pubs': pubs})
+    return render(request, 'blog/listar_pub.html', {'pubs': pubs})
